33d39aabd0f26217dd2b499975364d41b3040a09
Budaxcomel <96898071+Budaxcomel@users.noreply.github.com>
2024-08-28 13:05:09 +0800

    Update vars.py

--- YasirRoBot/vars.py
+++ YasirRoBot/vars.py
@@ -11,19 +11,19 @@ load_dotenv()
 class Var(object):
     MULTI_CLIENT = False
     API_ID = int(getenv("API_ID", "10115546"))
-    API_HASH = str(getenv("366347107f54aabc951cfa9d3c2fb2ce"))
-    BOT_TOKEN = str(getenv("7281537410:AAHsQ3joGTZvgbTFA7uj1oqaGy_4KM3fmsI"))
+    API_HASH = str(getenv("API_HASH", "366347107f54aabc951cfa9d3c2fb2ce"))
+    BOT_TOKEN = str(getenv("BOT_TOKEN", "7281537410:AAHsQ3joGTZvgbTFA7uj1oqaGy_4KM3fmsI"))
     name = str(getenv("name", "filetolinkbot"))
     SLEEP_THRESHOLD = int(getenv("SLEEP_THRESHOLD", "60"))
     WORKERS = int(getenv("WORKERS", "4"))
-    BIN_CHANNEL = int(getenv("BIN_CHANNEL"))
+    BIN_CHANNEL = int(getenv("BIN_CHANNEL", ""))
     PORT = int(getenv("PORT", 8080))
     BIND_ADRESS = str(getenv("WEB_SERVER_BIND_ADDRESS", "0.0.0.0"))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-    OWNER_ID = set(int(x) for x in os.environ.get("1358956715", "").split())
+    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "1358956715").split())
     NO_PORT = bool(getenv("NO_PORT", False))
     APP_NAME = None
-    OWNER_USERNAME = str(getenv("ownerimmanvpn"))
+    OWNER_USERNAME = str(getenv("OWNER_USERNAME", "ownerimmanvpn"))
     HASH_LENGTH = int(environ.get("HASH_LENGTH", 6))
     if not 5 < HASH_LENGTH < 64:
         sys.exit("Hash length should be greater than 5 and less than 64")
